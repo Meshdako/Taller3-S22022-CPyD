4d63c08d7c7aae4da631349b2164f491b87dc2e4
Meshdako <msobarzo@utem.cl>
2022-12-15 17:21:26 -0300

    Archivos agregados

--- cpyd_app_g1/lib/main.dart
+++ cpyd_app_g1/lib/main.dart
@@ -1,63 +1,24 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:cpyd_app_g1/src/pages/login_page.dart';
+import 'package:flutter_pokeapi_with_provider/home_page.dart';
+import 'package:flutter_pokeapi_with_provider/state_manager/pokemon_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Splash Screen',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-/* Splash Screen */
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-        const Duration(seconds: 5),
-        () => Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const InicioSesion())));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child:
-            const Image(image: AssetImage("lib/assets/images/Logo_UTEM.gif")));
-  }
-}
-
-/* Página de Inicio */
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Text('Hola'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<PokemonProvider>(
+          create: (_) => PokemonProvider(),
+        )
+      ],
+      child: const MaterialApp(home: HomePage()),
     );
   }
 }
