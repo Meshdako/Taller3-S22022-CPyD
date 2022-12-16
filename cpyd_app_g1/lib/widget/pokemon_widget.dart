87a2238991eb7a31a4104f4ec16befe6e4ea5582
Renato Palominos <rpalominos@utem.cl>
2022-12-16 11:27:12 -0300

    Comentario borrado

--- cpyd_app_g1/lib/widget/pokemon_widget.dart
+++ cpyd_app_g1/lib/widget/pokemon_widget.dart
@@ -50,56 +50,6 @@ class PokemonWidget extends StatelessWidget {
                 ),
               ),
             )
-            /* Positioned(
-              bottom: 1,
-              right: 1,
-              child: Image.network(
-                pokemon.sprites!.other!.officialArtwork!.frontDefault!,
-                fit: BoxFit.fill,
-                height: 70,
-                width: 70,
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "#${pokemon.id.toString()}",
-                ),
-                Text(
-                  pokemon.name!,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12,
-                      color: Colors.white),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: pokemon.types!
-                      .map(
-                        (e) => Container(
-                          margin: const EdgeInsets.only(bottom: 5),
-                          padding: const EdgeInsets.all(5),
-                          child: Text(
-                            e.type!.name!,
-                            style: const TextStyle(
-                              fontSize: 10,
-                              color: Colors.black,
-                            ),
-                          ),
-                          decoration: BoxDecoration(
-                            color: getBackGroundColor2(
-                              pokemon.types![0].type!.name!,
-                            ),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                )
-              ],
-            ) */
           ],
         ),
       ),
