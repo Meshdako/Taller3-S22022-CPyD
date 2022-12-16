4b85c45eb5d84dbcc693c92e14a80ac1f18dd989
Renato Palominos <rpalominos@utem.cl>
2022-12-16 00:39:12 -0300

    Actualización llamado inicio de sesión

--- cpyd_app_g1/lib/main.dart
+++ cpyd_app_g1/lib/main.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:cpyd_app_g1/home_page.dart';
-import 'package:cpyd_app_g1/state_manager/pokemon_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:cpyd_app_g1/screen/login.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<PokemonProvider>(
-          create: (_) => PokemonProvider(),
-        )
-      ],
-      child: const MaterialApp(home: HomePage()),
+    return MaterialApp(
+      title: 'POKEDEX',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: const MyHomePage(title: 'Autentificación con Correo UTEM'),
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const LoginScreen();
+  }
+}
+
