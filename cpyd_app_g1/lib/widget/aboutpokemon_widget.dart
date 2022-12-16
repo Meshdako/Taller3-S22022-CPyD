f6b01e0a5848c7fe30cd46a6fcfd68f1f5ff045c
Meshdako <msobarzo@utem.cl>
2022-12-16 18:15:11 -0300

    Actualizada vista de información del pokémon

--- cpyd_app_g1/lib/widget/aboutpokemon_widget.dart
+++ cpyd_app_g1/lib/widget/aboutpokemon_widget.dart
@@ -8,80 +8,47 @@ class AboutPokemonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Image(
-              image: AssetImage('lib/assets/Icons/Vector-1.png'),
-              ),
-            Image(
-              image: AssetImage('lib/assets/Icons/whitescreen.png'),
-              width: 25,
-              height: 20,
-              ),
-            Image(
-              image: AssetImage('lib/assets/Icons/Vector.png'),
-              ),
-            Image(
-              image: AssetImage('lib/assets/Icons/whitescreen.png'),
-              width: 25,
-              height: 20,
-              ),
-        ],),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text('Altura'),
-            SizedBox(height: 20),
-            Text('Peso'),
-            SizedBox(height: 20),
-            Text('Habilidad'),
-          ],
-        ),
-        const SizedBox(
-          width: 30,
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "${(pokemon.height! / 10).toString()} m",
-              ),
-              const SizedBox(height: 20),
-              Text(
-                "${(pokemon.weight! / 10).toString()} Kg",
-              ),
-              const SizedBox(height: 20),
-              Wrap(
-                children: pokemon.abilities!
-                    .map(
-                      (e) => Container(
-                        margin: const EdgeInsets.only(
-                          right: 10,
-                          bottom: 10,
-                        ),
-                        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                        decoration: BoxDecoration(
-                          color: getBackGroundColor(
-                            pokemon.types![0].type!.name!,
+    return Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.grey)),
+            margin: const EdgeInsets.only(right: 10),
+            padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            height: 100,
+            width: 400,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.monitor_weight,
+                              color: Colors.grey,
+                              size: 50,
+                            ),
+                            Text('${(pokemon.weight! / 10).toString()} Kg',
+                                style: const TextStyle(
+                                    fontSize: 25, color: Colors.black87))
+                          ],
+                        ))),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.center,
+                        child: Row(children: [
+                          const Icon(
+                            Icons.height,
+                            color: Colors.grey,
+                            size: 50,
                           ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(
-                          e.ability!.name!,
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    )
-                    .toList(),
-              ), 
-            ],
-          ),
-        ),
-      ],
-    );
+                          Text("${(pokemon.height! / 10).toString()} m",
+                              style: const TextStyle(
+                                  fontSize: 25, color: Colors.black87)),
+                        ]))),
+              ],
+            )));
   }
 }
