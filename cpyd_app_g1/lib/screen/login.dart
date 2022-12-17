83da002ec0dc0bc8e7d24a66e35d6d555e36514b
Renato Palominos <rpalominos@utem.cl>
2022-12-16 21:53:43 -0300

    Actualización Boton login

--- cpyd_app_g1/lib/screen/login.dart
+++ cpyd_app_g1/lib/screen/login.dart
@@ -7,34 +7,77 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          body: Stack(
-            children: [
-              const Image(
-                image: AssetImage('lib/assets/images/App_Flutter.png'),
-                fit: BoxFit.cover,
-                height: 1200,
-              ),
-              Expanded(
-                  child: Center(
-                child: SizedBox(
-                  height: 50,
-                  child: SignInButton(
-                    Buttons.Google,
-                    elevation: 5,
-                    text: 'Login UTEM',
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    onPressed: () => VoterService.login(context),
-                  ),
-                ),
-              ))
-            ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          const Image(
+            image: AssetImage('lib/assets/images/App_Flutter.png'),
+            fit: BoxFit.cover,
+            height: 1200,
           ),
-        )
-      ],
+          Expanded(
+              child: Center(
+            child: TextButton(
+              onPressed: () => VoterService.login(context),
+              child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                border: Border.all(color: Color(0xff4285f4), width: 1, ),
+                boxShadow: const [
+              BoxShadow(
+                color: Color(0xff4285f4),
+                blurRadius: 0,
+                offset: Offset(0, 4),
+              ),
+                ],
+                    color: Colors.white,
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14, ),
+                child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children:[
+                        SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children:[
+                                    Container(
+                                        width: 19.60,
+                                        height: 20,
+                                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(8),
+                                        ),
+                                        child: const Image(
+                                          image: AssetImage('lib/assets/Icons/google.png'),
+                                        ),
+                                    ),
+                                ],
+                            ),
+                        ),
+                        const SizedBox(width: 8),
+                        const Text(
+                            "Ingresar con Correo UTEM",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontFamily: "Helvetica Neue",
+                                fontWeight: FontWeight.w700,
+                                letterSpacing: 0.50,
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+            ),
+          ),
+          ),
+        ],
+      ),
     );
   }
 }
